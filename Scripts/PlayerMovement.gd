3beba597ebbe252cc0d253543885450c76c8e40a
Kamil Stefaniak <kstefan5@depaul.edu>
2023-11-09 22:10:01 -0600

    Added player gravity; object to collide and act as a floor for the player to land on; started on jump

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -2,20 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = 400.0
+const JUMP_VELOCITY = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const GRAVITY = 1 # num of pixels, edit to change speed of falling
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y += gravity * delta
+	# Player gravity
+	velocity.y += GRAVITY
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_up"): # and is_on_floor()
+		#velocity.y -= JUMP_VELOCITY
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
