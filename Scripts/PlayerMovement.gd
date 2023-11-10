8f9e9c6ddd6e179cd8e454e22de2f4acb80f2010
Kamil Stefaniak <kstefan5@depaul.edu>
2023-11-09 22:28:10 -0600

    -enabled Jump; -fixed jump to work only when touching floor; -modified Jump/Gravity values for "better experiance";

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -2,20 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = 100.0
+const JUMP_VELOCITY = 400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-const GRAVITY = 1 # num of pixels, edit to change speed of falling
+const GRAVITY = 10 # num of pixels, edit to change speed of falling
 
 func _physics_process(delta):
 	# Player gravity
 	velocity.y += GRAVITY
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up"): # and is_on_floor()
-		#velocity.y -= JUMP_VELOCITY
-		pass
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y -= JUMP_VELOCITY
+		#pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
